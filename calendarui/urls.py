1d2e6b27487f7ecfae13275e4b8c05390af5e351
iamllama <sahil.shukla@exiverprojects.com>
2020-05-05 23:48:52 +0530

    Added seperate page to display data based on filters from calanderui page.

--- calendarui/urls.py
+++ calendarui/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from calendarui.views import Calendar, GetEvents
+from calendarui.views import Calendar, GetEvents, TableView, GetTableData
 
 
 urlpatterns = [
     path('calendarUi', Calendar.as_view()),
-    path('get-events', GetEvents.as_view())
+    path('get-events', GetEvents.as_view()),
+    path('data-table', TableView.as_view()),
+    path('get-table-data', GetTableData.as_view()),
 ]
